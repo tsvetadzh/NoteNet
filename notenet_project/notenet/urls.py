2ac65c4db75fa5d4e21b89d22f879718d95f0701
Tsveta <tsvetadzhevelekova@gmail.com>
2026-05-02 09:49:36 +0300

    Added static and templates folders. Connected the frontend to Django and linked the pages.

--- notenet_project/notenet/urls.py
+++ notenet_project/notenet/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.homepage, name= 'homepage')
+    path('', views.homepage, name='homepage'),
+    path('', views.materials, name='materials'),
+    path('', views.addnote, name='addnote'),
+    path('', views.myclass, name='myclass'),
+    path('', views.profile, name='profile'),
 ]
--- notenet_project/notenet/views.py
+++ notenet_project/notenet/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
 
-# Create your views here.
 def homepage(request):
     return render(request, 'homepage.html')
+
+def addnote(request):
+    return render(request, 'addnote.html')
+
+def materials(request):
+    return render(request, 'materials.html')
+
+def myclass(request):
+    return render(request, 'myclass.html')
+
+def profile(request):
+    return render(request, 'profile.html')
